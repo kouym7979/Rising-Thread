34d97ae3452cd033f842bd5ad6e26276d5786f5b
Youngwoo Kim <52284829+kouym7979@users.noreply.github.com>
2021-01-22 23:48:52 +0900

    [add] displayMetrics

--- app/src/main/java/com/example/thread/MainActivity.kt
+++ app/src/main/java/com/example/thread/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.thread
 
 import android.animation.ObjectAnimator
+import android.hardware.display.DisplayManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.SystemClock
+import android.util.DisplayMetrics
 import android.util.Log
 import android.widget.ImageView
 import com.example.thread.databinding.ActivityMainBinding
@@ -14,7 +16,13 @@ class MainActivity : AppCompatActivity() {
     private var isRunning =true
     var handler:Handler = Handler(Looper.getMainLooper())
     var runnable:Runnable= Runnable {  }
+
+
     private lateinit var binding:ActivityMainBinding
+
+    val display =DisplayMetrics()
+    private var width : Int ?=null
+    private var height : Int ?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +30,22 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //val thread = ThreadClass()
+        val thread = ThreadClass()
 
-        DragonMove(binding.redDragon,600f,600f,2500L)
+        width=display.widthPixels
+        height=display.heightPixels
+        Log.d("확인","height:"+height)
+        thread.start()
+
 
     }
     fun DragonMove(image1 : ImageView, posX:Float, posY:Float,duration1:Long){
         runnable=object:Runnable{
             override fun run() {
-                ObjectAnimator.ofFloat(image1,"translationX",posX).apply {
+                /*ObjectAnimator.ofFloat(image1,"translationX",posX).apply {
                     duration=duration1
                     start()
-                }
+                }*/
                 ObjectAnimator.ofFloat(image1,"translationY",posY).apply {
                     duration=duration1
                     start()
@@ -46,10 +58,11 @@ class MainActivity : AppCompatActivity() {
     }
     //안드로이드에서 UI는 메인 쓰레드에서만 접근할 수 있다. 다른 쓰레드에서는 접근할 수 없다.
     //runOnUiThread메소드를 통해서 메인 쓰레드의 UI를 접근할 수 있다.
-    /*inner class ThreadClass:Thread(){
+    inner class ThreadClass:Thread(){
         override fun run(){
             while (isRunning){
                 SystemClock.sleep(100)//0.1초 간격
+                DragonMove(binding.redDragon, 0f,1000f,1000L)
                 Log.d("확인",System.currentTimeMillis().toString())
             }
         }
@@ -57,7 +70,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d("확인","종료되었습니다.")
         isRunning=false
-    }*/
+    }
 
 }
